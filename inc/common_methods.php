dd9f8aee3fb139384c58c41c618b6fde90473bec
vgerasim <gerkarasta@gmail.com>
2015-10-06 22:03:16 +0300

    Shit function added; secure method of transactions; fixes and updates;

--- inc/common_methods.php
+++ inc/common_methods.php
@@ -23,5 +23,37 @@ function register( $uid, $f_name, $s_name ) {
 	}
 }
 
+function createToken( $uid ) {
+	global $pdo;
+	$str = "";
+	for( $i = 0; $i < 20; $i++ ) {
+		$str .= rand( $i * 11, ( $i + 31 ) / 17 );
+	}
+	$token = md5( $str );
+	$q = "UPDATE `users` SET `token`='$token' WHERE `vkid`='$uid'";
+	$pdo->query( $q );
+	if( $pdo->errorCode() != 0 ) {
+		//error
+	}
+}
+
+function getToken( $uid ) {
+	global $pdo;
+	$q = "SELECT `token` FROM `users` WHERE `vkid`='$uid'";
+	$token = "";
+	foreach( $pdo->query( $q ) as $row ) {
+		$token = $row[ 'token' ];
+	}
+	return $token;
+}
+
+function toShit( $uid ) {
+	global $pdo;
+	$q = "UPDATE `users` SET `balance`=`balance`+100 WHERE `vkid`='$uid'";
+	$pdo->query( $q );
+	if( $pdo->errorCode() != 0 ) {
+		//error
+	}
+}
 
 ?>
